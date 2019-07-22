ac5c17e0197ce27fc9c53f716e7d035f55d03c10
ONL <chinook-media@gmx.de>
2019-07-22 11:28:26 +0200

    Update firstrun.sh

--- config/firstrun.sh
+++ config/firstrun.sh
@@ -7,6 +7,6 @@ MELLON_ENTITY_ID="${MELLON_ENDPOINT_URL}/metadata"
 
 /opt/mellon_create_metadata.sh $MELLON_ENTITY_ID $MELLON_ENDPOINT_URL
 
-mv *metadata.cert /etc/apache2/saml/metadata.cert
-mv *metadata.key /etc/apache2/saml/metadata.key
-mv *metadata.xml /etc/apache2/saml/metadata.xml
+mv *metadata.cert /etc/apache2/saml/mellon.crt
+mv *metadata.key /etc/apache2/saml/mellon.key
+mv *metadata.xml /etc/apache2/saml/mellon_metadata.xml
