3f79d24f96c9e2fae6a7013b26109d8793e882cf
ONL <chinook-media@gmx.de>
2019-11-15 16:16:11 +0100

    output content of generated files to screen

--- config/firstrun.sh
+++ config/firstrun.sh
@@ -8,5 +8,16 @@ MELLON_ENTITY_ID="${MELLON_ENDPOINT_URL}/metadata"
 /opt/mellon_create_metadata.sh $MELLON_ENTITY_ID $MELLON_ENDPOINT_URL
 
 mv *metadata.cert /etc/apache2/saml/mellon.crt
+echo mellon.crt
+echo ""
+cat /etc/apache2/saml/mellon.crt
+echo ""
 mv *metadata.key /etc/apache2/saml/mellon.key
+echo mellon.key
+echo ""
+cat /etc/apache2/saml/mellon.key
+echo ""
 mv *metadata.xml /etc/apache2/saml/mellon_metadata.xml
+echo mellon_metadata.xml
+echo ""
+cat /etc/apache2/saml/mellon_metadata.xml
